c42873f7d81c6d2f3a5523c340c8c1242b7236b1
Jay <psatdoctor@naver.com>
2019-05-04 10:21:20 +0900

    app req, res

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ const express = require('express');
 const app = express();
 
 
+//https://ko.wikipedia.org/wiki/HTTP_%EC%83%81%ED%83%9C_%EC%BD%94%EB%93%9C 
+//app이 인풋을 먹으면 아웃풋을 내보낸다. 
+//http200번을 응답하는 상황이되면 
+app.use((req, res) => {
+    res.status(200).json({
+        message: 'It works!'
+    });
+});
+
 //PORT는 방번호
 const PORT = 3000;
 
@@ -11,5 +20,6 @@ const PORT = 3000;
 const server = http.createServer(app);
 
 //서버가 시작하는 함수
+//http://localhost:3000 에서 이러한 작업이 일어난다. (포트에서)
 server.listen(PORT, console.log('serverStarted'));
 
