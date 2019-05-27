9d0b14230270e49e386f8e8da38e1968ff3b210f
Jay <psatdoctor@naver.com>
2019-05-27 20:24:50 +0900

    improved user signup

--- api/routes/users.js
+++ api/routes/users.js
@@ -10,35 +10,80 @@ const userModel = require('../models/user');
 //hash는 password를 보완하기 위해, 자동으로 암호화 시켜준다.
 router.post('/signup', (req, res) => {
 
-    bcrypt.hash(req.body.password, 10, (err, hash)=>{
-        if(err){
-            return res.status(500).json({
-                error: err
-            });
-        } else {
-            const user = new userModel({
-            _id: new mongoose.Types.ObjectId(),
-            userName: req.body.username,
-            email: req.body.email,
-            password: hash
+    userModel.find({ email: req.body.email })
+        .exec()
+        .then(user => {
+            if (user.length >= 1) {
+                return res.status(409).json({
+                    message: "Mail exists"
+                });
+            } else {
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    if (err) {
+                        return res.status(500).json({
+                            error: err
+                        });
+                    } else {
+                        const user = new userModel({
+                            _id: new mongoose.Types.ObjectId(),
+                            //userName은 출력항목이고, 내가 "": "요기"에 입력해야 하는 것은 req.body.username으로 설정해둔 값.
+                            userName: req.body.username,
+                            email: req.body.email,
+                            password: hash
+                        });
+
+                        user
+                            .save()
+                            .then(result => {
+                                res.status(200).json({
+                                    message: 'User created',
+                                    createdUser: result
+
+                                });
+                            })
+                            .catch(err => {
+                                res.status(500).json({
+                                    error: err
+                                });
+                            });
+                    }
+                });
+
+            }
         });
 
-        user
-            .save()
-            .then(result => {
-                res.status(200).json({
-                    message: 'User created',
-                    createdUser: result
 
-                });
-            })
-            .catch(err => {
-                res.status(500).json({
-                    error: err
-                });
-            });
-        }
-    }); 
+
+
+    // bcrypt.hash(req.body.password, 10, (err, hash)=>{
+    //     if(err){
+    //         return res.status(500).json({
+    //             error: err
+    //         });
+    //     } else {
+    //         const user = new userModel({
+    //         _id: new mongoose.Types.ObjectId(),
+    //         userName: req.body.username,
+    //         email: req.body.email,
+    //         password: hash
+    //     });
+
+    //     user
+    //         .save()
+    //         .then(result => {
+    //             res.status(200).json({
+    //                 message: 'User created',
+    //                 createdUser: result
+
+    //             });
+    //         })
+    //         .catch(err => {
+    //             res.status(500).json({
+    //                 error: err
+    //             });
+    //         });
+    //     }
+    // }); 
 
 
     // const user = new userModel({
@@ -65,7 +110,21 @@ router.post('/signup', (req, res) => {
 
 });
 
-
+router.delete('/:userId', (req, res) => {
+    userModel.remove({ _id: req.params.userId })
+        .exec()
+        .then(result => {
+            res.status(200).json({
+                message: "User deleted"
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+});
 
 
 module.exports = router;
